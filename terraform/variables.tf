eebb9bd36653147bce6afd5354bfe3e99fd039d0
rachelf27 <rachelvfmurphy@gmail.com>
2023-06-20 15:17:02 -0700

    adding install terraform to config.yml

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,9 @@
 variable "AWS_ACCESS_KEY" {
   description = "AWS access key"
+  default     = var.AWS_ACCESS_KEY != "" ? var.AWS_ACCESS_KEY : null
 }
 
 variable "AWS_SECRET_KEY" {
   description = "AWS secret key"
+    default     = var.AWS_SECRET_KEY != "" ? var.AWS_SECRET_KEY : null
 }
