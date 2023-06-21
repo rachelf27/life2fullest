930eb4e14015ef83ff76fb628412efea0457bc86
rachelf27 <rachelvfmurphy@gmail.com>
2023-06-21 16:38:11 -0700

    correcting region format

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      region                   = "us-east-1"
+      region          = "us-east-1"
       access_key      = var.AWS_ACCESS_KEY
       secret_key      = var.AWS_SECRET_KEY
     }
