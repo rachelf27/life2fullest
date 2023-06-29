1a6096992b552966b49af8639cd19cfd7ae4fa7b
rachelf27 <rachelvfmurphy@gmail.com>
2023-06-29 14:44:51 -0700

    adding MIME multipart message with bootscript

--- terraform/userData.sh
+++ terraform/userData.sh
@@ -1,3 +1,9 @@
+MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
+
+--==MYBOUNDARY==
+Content-Type: text/x-shellscript; charset="us-ascii"
+
 #!/bin/bash
 
 # Install Docker (if not already installed)
@@ -6,6 +12,9 @@ sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 
-cd /Users/rachelmurphy/Documents/Code/life2fullest
+pwd > /home/ec2-user/current_dir.txt
+
+cd /home/ec2-user/life2fullest
 /usr/local/bin/docker-compose up -d
 
+--==MYBOUNDARY==--
