d72f9b92f7a6d1b2d66d9b31a115cbe96a1e7f02
rachelf27 <rachelvfmurphy@gmail.com>
2023-07-10 15:56:46 -0700

    removing dynamodb as an object

--- data/populateData.js
+++ data/populateData.js
@@ -1,4 +1,5 @@
 const dynamoDb = require("../db/index");
+const { BatchWriteItemCommand } = require("@aws-sdk/client-dynamodb");
 const products = require("./initialData.json");
 
 const tableName = "ecom-app-products-table";
@@ -7,11 +8,11 @@ const populateData = async () => {
   const writeRequests = products.map((product) => ({
     PutRequest: {
       Item: {
-        productId: product.productId,
-        name: product.name,
-        price: product.price,
-        description: product.description,
-        imageUrl: product.imageUrl,
+        productId: { S:product.productId},
+        name: { S: product.name},
+        price: { N: product.price.toString()},
+        description: {S: product.description},
+        imageUrl: {S: product.imageUrl},
       },
     },
   }));
@@ -22,8 +23,10 @@ const populateData = async () => {
     },
   };
 
+  const command = new BatchWriteItemCommand(params);
+
   try {
-    await dynamoDb.batchWrite(params).promise();
+    await dynamoDb.send(command);
     console.log("Data populated successfully");
   } catch (error) {
     console.error("Error populating data:", error);
--- db/index.js
+++ db/index.js
@@ -1,11 +1,7 @@
-//const AWS = require('aws-sdk');
-//const dynamoDb = new AWS.DynamoDB.DocumentClient();
+// db/index.js
 
-// New AWS SDK v3
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const { DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
-const dynamoDb = DynamoDBDocumentClient.from(new DynamoDBClient({ region: "us-east-1" }));
-
 const config = require('../config/config');
-AWS.config.update(config.aws_remote_config);
+const dynamoDb = new DynamoDBClient(config.aws_remote_config);
+
 module.exports = dynamoDb;
